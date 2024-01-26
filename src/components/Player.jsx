dafca0789866c44aa95b3cbd3e4550c52f27e31f
Karunlal <karunlalp@gmail.com>
2024-01-26 17:03:33 +0530

    first commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+const Player = ({ initialName, symbol }) => {
+  const [playerName, setPlayerName] = useState(initialName)
+  const [isEditing, setIsEditing] = useState(false)
+  const handleEditClick = () => {
+    setIsEditing((editing) => !editing)
+  }
+
+  const handleChange = (event) => {
+    console.log(event)
+    setPlayerName(event.target.value)
+  }
+  let editablePlayerName = <span className="player-name">{playerName}</span>
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    )
+  }
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  )
+}
+
+export default Player
