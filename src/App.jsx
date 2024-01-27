737a7b48a7e3b4f02aa3e6208800c9a7bb16076d
Karunlal <karunlalp@gmail.com>
2024-01-27 11:16:26 +0530

    rewrite

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-import { useState } from 'react'
-import Player from './components/Player'
-import GameBoard from './components/GameBoard'
-
-function App() {
-  const [activePlayer, setActivePlayer] = useState('X')
-
-  function handleSelectSquare() {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'))
-  }
+const App = () => {
   return (
     <main>
       <div id="game-container">
-        <ol id="players" className="highlight-player">
-          <Player
-            initialName="Player 1"
-            symbol="X"
-            isActive={activePlayer === 'X'}
-          />
-          <Player
-            initialName="Player 2"
-            symbol="O"
-            isActive={activePlayer === 'O'}
-          />
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
-        />
+        GAME BOARD
       </div>
       LOG
     </main>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,29 +1,12 @@
 import { useState } from 'react'
-
-const Player = ({ initialName, symbol, isActive }) => {
-  const [playerName, setPlayerName] = useState(initialName)
-  const [isEditing, setIsEditing] = useState(false)
-  const handleEditClick = () => {
-    setIsEditing((editing) => !editing)
-  }
-
-  const handleChange = (event) => {
-    console.log(event)
-    setPlayerName(event.target.value)
-  }
-  let editablePlayerName = <span className="player-name">{playerName}</span>
-  if (isEditing) {
-    editablePlayerName = (
-      <input type="text" required value={playerName} onChange={handleChange} />
-    )
-  }
+const Player = ({ name, symbol }) => {
   return (
-    <li className={isActive ? 'active' : undefined}>
+    <li>
       <span className="player">
-        {editablePlayerName}
+        <span className="player-name">{name}</span>
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+      <button>Edit</button>
     </li>
   )
 }
